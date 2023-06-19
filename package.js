a1292450663637ac0a86aaf7822c04380aad1bd9
Kajal-gajjar <kngajjar123@gmail.com>
2023-06-19 18:47:25 +0530

    zip file added

--- package.js
+++ package.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const archiver = require('archiver');
 const AWS = require('aws-sdk');
+const glob = require('glob');
 
 const s3 = new AWS.S3();
 
@@ -25,5 +26,19 @@ output.on('close', () => {
 });
 
 archive.pipe(output);
-archive.directory('.serverless/', false);
+
+// Get all files and directories inside the 'code' folder
+const files = glob.sync('code/**', { dot: true });
+
+// Add files and directories to the archive while maintaining the directory structure
+files.forEach((file) => {
+  const stats = fs.statSync(file);
+  if (stats.isFile()) {
+    const fileContent = fs.readFileSync(file);
+    archive.append(fileContent, { name: file.replace('code/', '') });
+  } else if (stats.isDirectory()) {
+    archive.directory(file, file.replace('code/', ''));
+  }
+});
+
 archive.finalize();
